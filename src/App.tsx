42ffa999e13f68457b52a48966f2fc2f19a58695
Ganeshkaredla <ganeshkaredla6@gmail.com>
2020-05-19 21:31:33 +0530

    added assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
 import React from "react";
-import {
-  HashRouter,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Home from "./eCommerceApp/components/home";
-import eCommerceRoutes from "./eCommerceApp/routes/eCommerceRoutes";
-import { Provider } from "mobx-react";
-import stores from "./eCommerceApp/stores";
-import SignInPage from "./authentication/components/SignInRoute";
-import PracticeAdvancedConceptsRoute from "./common/routes/PracticeAdvancedConceptsRoute/index";
 
 class App extends React.Component{
   render(){
   return (
-    <Provider {...stores}>
-      <HashRouter>
-        <Switch>
-          {eCommerceRoutes}
-          <Route path='/ecommerce-store/sign-in' component={SignInPage} />
-          <Route path='/practice-advanced-concepts'>
-            <PracticeAdvancedConceptsRoute />
-          </Route>
-          <Route path="/">
-          <Home />  
-          </Route>
-          </Switch>
-      </ HashRouter>
-    </Provider>
+   <div>Hello world</div>
   );
   }
 }
